47175ff2811b3f61af64926c2a645cf5c795689d
jibrail0398 <izamikatsuka@gmail.com>
2025-10-20 10:12:26 +0700

    Ganti route dan edit gitignore

--- routes/web.php
+++ routes/web.php
@@ -23,27 +23,3 @@ Route::middleware(['auth'])->group(function(){
 });
 
 
-
-// login
-// Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
-// Route::post('/login', [LoginController::class, 'login']);
-// =========================
-
-// Route::get('/form', [FormController::class, 'showForm'])->name('form');
-// Route::post('/form/submit', [FormController::class, 'storeScore'])->name('storeScore');
-
-// Route::get('/scan', [ExpoController::class, 'scan']);
-// Route::post('/absen', [ExpoController::class, 'absen']);
-// Route::get('/sertifikat/{hash}', [ExpoController::class, 'sertifikat'])->name('sertifikat');
-// Route::get('/admin', [ExpoController::class, 'admin']);
-// Route::get('/generateqrcode/{pesertaId}', [ExpoController::class, 'generateQRCode']);
-// Route::get('/sertifikat-saya', [ExpoController::class, 'sertifikatView']);
-// Route::post('/cek-sertifikat', [ExpoController::class, 'sertifikat'])->name('sertifikat-saya');
-
-// Route::get('/admin/kelompok/{id}/tampilkan', [ExpoController::class, 'tampilkanKelompok'])->name('admin.tampilkanKelompok');
-
-// Route::get('/penilaian', PenilaianComponent::class)->name('penilaian');
-// Route::get('/kontestan', [ExpoController::class, 'listKontestan'])->name('kontestan');
-
-
-// Route::post('/admin/import/kontestan', [ExpoController::class, 'import'])->name('admin.import.kontestan');
